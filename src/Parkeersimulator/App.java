09f03d769683a1b94182e493e9536aee6ed25f31
Faimire <faimireg@hotmail.com>
2018-01-22 19:07:00 +0100

    Now you can use window builder

--- src/Parkeersimulator/App.java
+++ src/Parkeersimulator/App.java
@@ -37,7 +37,7 @@ public class App {
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		simulatorview = new SimulatorView(3, 6, 30);
-		frame.add(simulatorview.getContentPane());
+		frame.add(simulatorview.getRootPane());
 		frame.pack();
 		Dimension dim = Toolkit.getDefaultToolkit().getScreenSize();
 		frame.setLocation(dim.width/2-frame.getSize().width/2, dim.height/2-frame.getSize().height/2);
